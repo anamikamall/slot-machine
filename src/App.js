348fbe41a99f7d113b49c855eb01945a1e4f60c7
Anamika Mall <anamikamall221@gmail.com>
2023-01-01 05:33:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SlotM from './SlotMach';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+return (
+  <>
+  <div className='heading'>
+  <h1>
+    🎰 Welcome to <span style={{fontWeight: 'bold'}}>Slot Machine Game</span> 🎰
+    </h1>
     </div>
-  );
-}
-
+    <div className='slotmach'>
+      <SlotM x='😄' y='😄' z='😄'/>
+      <hr/>
+      <SlotM x='😄' y='😆' z='😄'/>
+      <hr/>
+      <SlotM x='🍎' y='🍌' z='🍎'/>
+      <hr/>
+      <SlotM x='💑' y='💑' z='💑'/>
+    </div>
+  </>
+);
+};
 export default App;
